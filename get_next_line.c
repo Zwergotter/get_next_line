24cd71ded3c86bb5c6d388fe3376396b03dc68c0
Esther DEVEZE <edeveze@student.42.fr>
2017-01-12 12:16:47 +0100

    header corresponding at the 42 norm, file c still need a bit of work

--- get_next_line.c
+++ get_next_line.c
@@ -6,74 +6,74 @@
 /*   By: edeveze <edeveze@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/18 17:32:13 by edeveze           #+#    #+#             */
-/*   Updated: 2017/01/09 15:09:07 by edeveze          ###   ########.fr       */
+/*   Updated: 2017/01/12 12:13:21 by edeveze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-int find_character(char *str)
+int		find_character(char *str)
 {
-    int i;
+	int	i;
 
-    i = 1;
-    while (*str && *str != '\n')
-        {
-            i++;
-            str++;
-        }
-    if (*str == '\n')
-        return (i);
-    return (0);
+	i = 1;
+	while (*str && *str != '\n')
+	{
+		i++;
+		str++;
+	}
+	if (*str == '\n')
+		return (i);
+	return (0);
 }
 
-void line_read(char **line, char **saved)
+void	line_read(char **line, char **saved)
 {
-    int  newline;
-    char *temp;
+	int		newline;
+	char	*temp;
 
-    temp = *saved;
-    newline = find_character(temp);
-    if (newline == 1 || newline == 0)
-        *line = ft_strnew(0);
-    else
-        *line = ft_strsub(temp, 0, newline - 1);
-    // *saved = ft_strsub(temp, newline, ft_strlen(temp) - (newline - 1));
-    *saved = ft_strdup(temp + newline);
-    free(temp);
+	temp = *saved;
+	newline = find_character(temp);
+	if (newline == 1 || newline == 0)
+		*line = ft_strnew(0);
+	else
+		*line = ft_strsub(temp, 0, newline - 1);
+	//*saved = ft_strsub(temp, newline, ft_strlen(temp) - (newline - 1));
+	*saved = ft_strdup(temp + newline);
+	free(temp);
 }
 
-int get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line)
 {
-    static char *saved = NULL;
-    char buf[BUFF_SIZE + 1];
-    char *tmp;
-    int ret;
+	static char	*saved = NULL;
+	char		buf[BUFF_SIZE + 1];
+	char		*tmp;
+	int			ret;
 
-    if (fd < 0)
-        return (-1);
-    while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
-    {
-        buf[ret] = '\0';
-        tmp = saved;
-        saved = ft_strjoin(saved, buf);
-        if (saved == NULL)
-            return (-1);
-        if (tmp)
-            free(tmp);
-        if (ft_strchr(saved, '\n'))
-            break;
-    }
-    line_read(line, &saved);
+	if (fd < 0)
+		return (-1);
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	{
+		buf[ret] = '\0';
+		tmp = saved;
+		saved = ft_strjoin(saved, buf);
+		if (saved == NULL)
+			return (-1);
+		if (tmp)
+			free(tmp);
+		if (ft_strchr(saved, '\n'))
+			break ;
+	}
+	line_read(line, &saved);
 	// printf("ret = %d | line = \"%s\" | saved = \"%s\"\n", ret, *line, saved);
-    if (ret <= 0 && **line == '\0' && *saved == '\0')
-    {
-        free(saved);
+	if (ret <= 0 && **line == '\0' && *saved == '\0')
+	{
+		free(saved);
 		saved = NULL;
 		free(*line);
 		*line = NULL;
-        return (0);
-    }
-    return (1);
+		return (0);
+	}
+	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,16 @@
 /*   By: edeveze <edeveze@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/29 14:50:23 by edeveze           #+#    #+#             */
-/*   Updated: 2017/01/02 17:25:17 by edeveze          ###   ########.fr       */
+/*   Updated: 2017/01/12 12:14:53 by edeveze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <sys/uio.h>
-#include <stdlib.h>
-#include "libft.h"
+# include <sys/uio.h>
+# include <stdlib.h>
+# include "libft.h"
 
 # define BUFF_SIZE 10
 
